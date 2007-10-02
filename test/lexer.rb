a9acb037298abb9340ee03534673d96525621c91
Gregory Murphy <gregory.murphy@sun.com>
2007-10-02 17:01:33 +0000

    Enhancing lexer test, to check for both "\n" and "\r\n" in various conditions.
    Also modified file comparison test so that expected result is first argument, and a message including the file name is output upon a test failure. This will make test failures easier to understand.
    
    
    
    git-svn-id: http://mediacloth.rubyforge.org/svn/trunk@40 ed6bb6d2-8713-0410-a66e-f7c184f9b1ef

--- test/lexer.rb
+++ test/lexer.rb
@@ -6,17 +6,34 @@ class Lexer_Test < Test::Unit::TestCase
 
     include TestHelper
 
-    def test_input
+    def test_standard_formatted_input
         test_files("lex") { |input,result,resultname|
             lexer = MediaWikiLexer.new
             tokens = lexer.tokenize(input)
-            assert_equal(tokens.to_s, result)
+            assert_equal(result, tokens.to_s, "Mismatch in #{resultname}")
+        }
+    end
+    
+    def test_internet_formatted_input
+        test_files("lex") { |input,result,resultname|
+            lexer = MediaWikiLexer.new
+            tokens = lexer.tokenize(input.gsub("\n", "\r\n"))
+            assert_equal(result.gsub("\n", "\r\n"), tokens.to_s, "Mismatch in #{resultname}")
         }
     end
 
     def test_paragraphs
-        assert_equal(lex("Before\n\n\n=Headline="),
+        assert_equal(lex("text\n\ntext"),
+            [[:PARA_START, ""], [:TEXT, "text"], [:PARA_END, "\n\n"], 
+              [:PARA_START, ""], [:TEXT, "text"], [:PARA_END, ""], [false,false]])
+        assert_equal(lex("text\r\n\r\ntext"),
+            [[:PARA_START, ""], [:TEXT, "text"], [:PARA_END, "\r\n\r\n"], 
+              [:PARA_START, ""], [:TEXT, "text"], [:PARA_END, ""], [false,false]])
+        assert_equal(lex("Before\n\n=Headline="),
             [[:PARA_START, ""], [:TEXT, "Before"], [:PARA_END, "\n\n"],
+             [:SECTION_START, "="], [:TEXT, "Headline"], [:SECTION_END, "="], [false,false]])
+        assert_equal(lex("Before\r\n\r\n=Headline="),
+            [[:PARA_START, ""], [:TEXT, "Before"], [:PARA_END, "\r\n\r\n"],
              [:SECTION_START, "="], [:TEXT, "Headline"], [:SECTION_END, "="], [false,false]])
     end
 
@@ -27,6 +44,7 @@ class Lexer_Test < Test::Unit::TestCase
     def test_preformatted
         #assure preformatted text works as expected at the start of the text
         assert_equal(lex(" Foo\n"), [[:PRE, "Foo\n"], [false, false]])
+        assert_equal(lex(" Foo\r\n"), [[:PRE, "Foo\r\n"], [false, false]])
         assert_equal(lex(" Foo"), [[:PRE, "Foo"], [false, false]])
     end
 
@@ -34,6 +52,7 @@ class Lexer_Test < Test::Unit::TestCase
         #assure that at the start of the text hline still works
         assert_equal(lex("----"), [[:HLINE, "----"], [false, false]])
         assert_equal(lex("\n----"), [[:HLINE, "----"], [false, false]])
+        assert_equal(lex("\r\n----"), [[:HLINE, "----"], [false, false]])
     end
 
     def test_ending_text_token
@@ -41,8 +60,14 @@ class Lexer_Test < Test::Unit::TestCase
         assert_equal(lex("\n----\nfoo\n"),
             [[:HLINE, "----"], [:PARA_START, ""],
                 [:TEXT, "\nfoo\n"], [:PARA_END, ""], [false, false]])
+        assert_equal(lex("\r\n----\r\nfoo\r\n"),
+            [[:HLINE, "----"], [:PARA_START, ""],
+                [:TEXT, "\r\nfoo\r\n"], [:PARA_END, ""], [false, false]])
         assert_equal(lex("\n----\nfoo\n Hehe"),
             [[:HLINE, "----"], [:PARA_START, ""], [:TEXT, "\nfoo\n"],
+                [:PARA_END, ""], [:PRE, "Hehe"], [false, false]])
+        assert_equal(lex("\r\n----\r\nfoo\r\n Hehe"),
+            [[:HLINE, "----"], [:PARA_START, ""], [:TEXT, "\r\nfoo\r\n"],
                 [:PARA_END, ""], [:PRE, "Hehe"], [false, false]])
     end
 
