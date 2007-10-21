0df3f3112148fe6f918d1482070e20c6f8529183
Gregory Murphy <gregory.murphy@sun.com>
2007-10-21 15:39:42 +0000

    Modified test to output name of file being processed, to make debugging a little easier.
    git-svn-id: http://mediacloth.rubyforge.org/svn/trunk@64 ed6bb6d2-8713-0410-a66e-f7c184f9b1ef

--- test/htmlgenerator.rb
+++ test/htmlgenerator.rb
@@ -10,14 +10,15 @@ class HTMLGenerator_Test < Test::Unit::TestCase
     include TestHelper
 
     def test_input
-        test_files("html") { |input,result|
+        test_files("html") { |input,result,name|
+            puts name
             parser = MediaWikiParser.new
             parser.lexer = MediaWikiLexer.new
             ast = parser.parse(input)
             MediaWikiParams.instance.time = Time.utc(2000, 1, 1, 1, 1, 1, 1)
             generator = MediaWikiHTMLGenerator.new
             generator.parse(ast)
-            assert_equal generator.html, result
+            assert_equal generator.html, result, name
 #             puts generator.html
         }
     end
