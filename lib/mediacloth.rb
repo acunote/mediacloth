b8985489fc5c6a903ae3c860b8da12bc31b4e407
Thiago Arrais <thiago.arrais@gmail.com>
2008-02-15 02:38:24 +0000

    Introduces options hash into the wiki_to_html command for replacing the default link handler and html generator
    TODO: Update docs and tests!!!
    
    
    git-svn-id: http://mediacloth.rubyforge.org/svn/trunk@116 ed6bb6d2-8713-0410-a66e-f7c184f9b1ef

--- lib/mediacloth.rb
+++ lib/mediacloth.rb
@@ -9,16 +9,16 @@ require 'mediacloth/mediawikilinkhandler'
 #Helper module to facilitate MediaCloth usage.
 module MediaCloth
 
-    #Parses wiki formatted +input+ and generates its html representation.
-    def wiki_to_html(input)
-        parser = MediaWikiParser.new
-        parser.lexer = MediaWikiLexer.new
-        ast = parser.parse(input)
-        walker = MediaWikiHTMLGenerator.new
-        walker.parse(ast)
-        walker.html
-    end
+  #Parses wiki formatted +input+ and generates its HTML representation.
+  def wiki_to_html(input, options={})
+    parser = MediaWikiParser.new
+    parser.lexer = MediaWikiLexer.new
+    tree = parser.parse(input)
+    generator = options[:generator] || MediaWikiHTMLGenerator.new
+    generator.link_handler = options[:link_handler] if options[:link_handler]
+    generator.parse(tree)
+  end
 
-    module_function :wiki_to_html
+  module_function :wiki_to_html
 
 end
