96e1c1110015a86ffb55ba33fd467fa4f94f8c2e
Thiago Arrais <thiago.arrais@gmail.com>
2008-02-19 20:21:24 +0000

    Updates docs with new options hash for wiki_to_html
    --- pt-br -----------------
    
    Atualiza documentacao com a nova hash de opcoes para wiki_to_html
    
    
    git-svn-id: http://mediacloth.rubyforge.org/svn/trunk@117 ed6bb6d2-8713-0410-a66e-f7c184f9b1ef

--- lib/mediacloth.rb
+++ lib/mediacloth.rb
@@ -10,6 +10,11 @@ require 'mediacloth/mediawikilinkhandler'
 module MediaCloth
 
   #Parses wiki formatted +input+ and generates its HTML representation.
+  #
+  #Can reveive options for customizing the HTML renderer and link_handler. These
+  #are the available options:
+  #  :generator => An HTML generator (see MediaWikiHTMLGenerator)
+  #  :link_handler => A link handler (see MediaWikiLinkHandler)
   def wiki_to_html(input, options={})
     parser = MediaWikiParser.new
     parser.lexer = MediaWikiLexer.new
